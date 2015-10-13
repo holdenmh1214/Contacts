50e5ea12543074e0778e43a46f67bedf643c58d2
holdenhughes <holdenmh121489@gmail.com>
2015-10-13 10:21:33 -0400

    Add test code to practice constructors and data structures

--- src/Contact.java
+++ src/Contact.java
@@ -7,27 +7,8 @@ public class Contact {
     public String email;
     public int age;
 
-    public Contact(){
-        this.name = "Holden Hughes";
-        this.email = "holden@awesome.com";
-    }
-
-    public Contact(String name, String address, String email, int age){
-        this.name = name;
-        this.address = address;
-        this.email = email;
-        this.age = age;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String newName) {
-        this.name = newName;
-    }
-
-    public static boolean isValidName(String name){
-        return name.length() !=0;
+    public Contact(String name, int age){
+        this.name=name;
+        this.age=age;
     }
 }
---
+++ src/Contacts.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by holdenhughes on 10/13/15.
+ */
+public class Contacts {
+    public static void main(String[] args) {
+        Contact bob = new Contact("Bob", 30);
+        Contact alice = new Contact("Alice", 20);
+        Contact charlie = new Contact("Charlie", 40);
+
+        ArrayList<Contact> contacts = new ArrayList();
+        contacts.add(bob);
+        contacts.add(alice);
+        contacts.add(charlie);
+
+        ArrayList<Contact> contactsUnderThirty = new ArrayList();
+
+
+        Contact contactToFind = null;
+        for (Contact c : contacts) {
+            if (c.age<30){
+                contactToFind = c;
+                break;
+            }
+        }
+        System.out.println(contactsUnderThirtD
+        ;
+
+        HashMap<String, Contact> contactMap = new HashMap();
+        contactMap.put("Bob", bob);
+        contactMap.put("Alice", alice);
+        contactMap.put("Charlie", charlie);
+
+        //contactToFind = contactMap.get("Alice");
+
+        if (contactToFind == null) {
+            System.out.println("Contact not found");
+
+        } else {
+            System.out.println("Found Contact");
+        }
+
+    }
+}
