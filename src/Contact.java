caf34d217a69076c4145e7abd643881263d9347a
holdenhughes <holdenmh121489@gmail.com>
2015-10-19 11:44:51 -0400

    Implement comparable interface

--- src/Contact.java
+++ src/Contact.java
@@ -1,19 +1,21 @@
+import java.util.Comparator;
+
 /**
  * Created by holdenhughes on 10/7/15.
  */
-public class Contact {
+public class Contact implements Comparable {
     public String name;
     public String address;
     public String email;
     public int age;
 
-    public Contact(){
+    public Contact() {
 
     }
 
-    public Contact(String name, int age){
-        this.name=name;
-        this.age=age;
+    public Contact(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public String getName() {
@@ -30,5 +32,16 @@ public class Contact {
 
     public int getAge() {
         return age;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Contact c = (Contact) o;
+        int result = name.compareTo(c.name);
+        if (result == 0) {
+            return age - c.age;
+        } else {
+            return result;
+        }
     }
 }
